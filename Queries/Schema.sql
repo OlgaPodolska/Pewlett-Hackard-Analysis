9dacae559e617d7e90a28037ad115dbf79eec686
Olga Podolska <olgapodolska@Olgas-Air-2.attlocal.net>
2022-05-19 17:43:18 -0700

    Retirement info upload

--- Queries/Schema.sql
+++ Queries/Schema.sql
@@ -31,4 +31,26 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
 SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
